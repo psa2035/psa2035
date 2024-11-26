ce6c94bf7e5d53fe099ba14c71292987406a5d2d
pankaj <pankajsiracademy2027@gmail.com>
2024-11-26 19:32:01 +0530

    defect-fixed-3245: Wass return null value as response in postman.

--- src/main/java/com/crm/entity/Comment.java
+++ src/main/java/com/crm/entity/Comment.java
@@ -23,4 +23,35 @@ public class Comment {
     @JoinColumn(name = "post_id")
     private Post post;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Post getPost() {
+        return post;
+    }
+
+    public void setPost(Post post) {
+        this.post = post;
+    }
 }
